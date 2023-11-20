f9d0545ed997adba5475c25b1724514e238f7dc4
euLita <litas.educ@gmail.com>
2023-11-20 19:24:09 -0400

    well, today i did the first test console to check of values on input and, creation one function

--- script.js
+++ script.js
@@ -1,2 +1,11 @@
 
-alert("very goood!")
+//alert("very goood!")
+
+const button = document.querySelector('.button-add-task')
+const input = document.querySelector('.input-task')
+
+function getValueInput() {
+    console.log(input.value)
+}
+
+button.addEventListener('click', getValueInput)
