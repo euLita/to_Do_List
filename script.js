620506be99d05ea93c1528b448f93d28aa42ae85
euLita <litas.educ@gmail.com>
2023-11-20 19:54:27 -0400

    now, i created or, better say i renamed the function i created at first and, declared an variable, type array to get the values to store where the method button.addEventListener check of input the news tasks

--- script.js
+++ script.js
@@ -1,11 +1,19 @@
 
-//alert("very goood!")
+/*alert("very goood!")*/
 
 const button = document.querySelector('.button-add-task')
 const input = document.querySelector('.input-task')
-
+/*
 function getValueInput() {
     console.log(input.value)
+}*/
+
+let myListItems = []
+
+function addNewTask () {
+    myListItems.push(input.value)
+
+    console.log(myListItems);
 }
 
-button.addEventListener('click', getValueInput)
+button.addEventListener('click', addNewTask)
